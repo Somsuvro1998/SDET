58fb2427dfb607426e508dabfdaf2daba92a1643
Somsuvro1998 <Palz.somsuvro@gmail.com>
2024-06-19 19:35:44 +0530

    2nd push

---
+++ src/test/java/practice_sleneium/SampleTest.java
@@ -0,0 +1,15 @@
+package practice_sleneium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class SampleTest {
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+	driver.get("https://www.google.com/");	
+		System.out.println("Say Hi");
+	}
+}
